5fa1b4025b0c09ca108d92d9d8986d29061a0117
2재 <111326830+jjootf@users.noreply.github.com>
2024-05-29 17:41:58 +0900

    Update 0529-3.js

--- 0529-3.js
+++ 0529-3.js
@@ -1,13 +1,10 @@
 const $popup = document.querySelector("#popup");
 $popup.children[1].addEventListener("click", dat);
 
-const $footer = document.querySelector("#popup button");
-$footer.addEventListener("click", bo);
-
 function dat(){
     $popup.style.display = "none";
 }
 
 function bo(){
-    $footer.style.display = "none";
+    $popup.style.display = "block";
 }
